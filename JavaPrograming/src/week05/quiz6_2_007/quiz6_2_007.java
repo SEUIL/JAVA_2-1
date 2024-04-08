0dee9e47cf3ac3b06ebbca4d0e4e7aca849fbccb
SEUIL <sone91961@gmail.com>
2024-04-08 23:54:42 +0900

    4월8일 HOME

--- JavaPrograming/src/week05/quiz6_2_007/quiz6_2_007.java
+++ JavaPrograming/src/week05/quiz6_2_007/quiz6_2_007.java
@@ -1,5 +1,10 @@
 package week05.quiz6_2_007;
 
+//다음 표와 실행 결과를 참고해서 프로그램을 작성하시오. Show()메서드는 객체의 정보를 문자열로 반환한다.
+// - Person, Person의 자식 Student, Student의 자식 ForeignStudent클래스를 작성하시오.
+// - Person 타입 배열이 Person, Student, ForeignStudent 타입의 객체를 1개씩 포함하며, Person타입 배열
+//원소를 for ~ each 문을 사용해 각 원소의 정보를 실행 결과와 같이 출력하는 프로그램을 작성하시오.
+
 public class quiz6_2_007 {
     //2133007
     //황스일
@@ -14,8 +19,8 @@ public class quiz6_2_007 {
 }
 
 class Person{
-    public String name;
-    public int age;
+    protected String name;
+    protected int age;
 
     Person(String name,int age){
         this.name = name;
@@ -29,7 +34,7 @@ class Person{
 
 class Student extends Person{
 
-    public int studentNumber;
+    protected int studentNumber;
 
     Student(String name,int age,int studentNumber){
         super(name,age);
@@ -43,7 +48,7 @@ class Student extends Person{
 }
 
 class ForeignStudent extends Student{
-    public String nationality;
+    protected String nationality;
 
     ForeignStudent(String name,int age,int studentNumber, String nationality){
         super(name,age,studentNumber);
